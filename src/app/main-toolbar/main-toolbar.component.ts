04584f8a41fa64a244ac618979b117b81b90905d
Alberto Torres <alberto.torres@gft.com>
2023-03-23 13:59:24 +0100

    componentized main app toolbar

---
+++ src/app/main-toolbar/main-toolbar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login/login.service';
+
+@Component({
+  selector: 'app-main-toolbar',
+  templateUrl: './main-toolbar.component.html',
+  styleUrls: ['./main-toolbar.component.scss']
+})
+export class MainToolbarComponent implements OnInit {
+
+  constructor(public loginService: LoginService) {
+  }
+
+  ngOnInit() {
+    this.loginService.showLogOutIcon = localStorage.getItem('accessKey') && localStorage.getItem('accessKey') !== '' ? true : false;
+  }
+
+  public logOut() {
+    this.loginService.logOut();
+  }
+
+  public get showLogOutIcon() {
+    return this.loginService.showLogOutIcon;
+  }
+
+}
