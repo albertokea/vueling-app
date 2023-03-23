2ce18f546b5feab2312aa13a3bf712b57bf2874d
Alberto Torres <alberto.torres@gft.com>
2023-03-23 15:34:18 +0100

    ng update

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
 import { MatCardModule } from "@angular/material/card";
 import { LoginComponent } from "./login.component";
 import { LoginRoutingModule } from "./login.routing.module";
 import { ReactiveFormsModule } from "@angular/forms";
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from "@angular/common";
 
 
 
