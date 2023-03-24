df629b26f7a76aff053950fcb9e6cebf3698860a
Alberto Torres <alberto.torres@gft.com>
2023-03-24 12:56:59 +0100

    login guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RoutesGuard } from './guards/routes.guard';
+import { LoginGuard } from './guards/login/login.guard';
+import { RoutesGuard } from './guards/routes/routes.guard';
 
 const routes: Routes = [
   {
@@ -11,6 +12,7 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./components/login/login.module').then((m) => m.LoginModule),
+    canActivate: [LoginGuard]
   },
   {
     path: 'airportsList',
