6acd818726658d8890e0a636752c2c4df9635bcd
Alberto Torres <alberto.torres@gft.com>
2023-03-24 13:05:52 +0100

    change airport-list layout

--- src/app/airports-list/airports-list.module.ts
+++ src/app/airports-list/airports-list.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AirportsListComponent } from "./airports-list.component";
-import { AirportCardComponent } from "./airport-card/airport-card.component";import { MatCardModule } from "@angular/material/card";import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { AirportCardComponent } from "./airport-card/airport-card.component";
+import { MatCardModule } from "@angular/material/card";
 import { AirportsListRoutingModule } from "./airports-list.routing.module";
+import { AirportDetailComponent } from "./airport-detail/airport-detail.component";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
-  declarations: [AirportsListComponent, AirportCardComponent],
+  declarations: [AirportsListComponent, AirportCardComponent, AirportDetailComponent],
   imports: [
     CommonModule,
-    MatCardModule,
-    MatProgressSpinnerModule,
+    SharedModule,
     AirportsListRoutingModule
   ],
 })
--- src/app/airports-list/airports-list.routing.module.ts
+++ src/app/airports-list/airports-list.routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AirportDetailComponent } from './airport-detail/airport-detail.component';
 import { AirportsListComponent } from './airports-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AirportsListComponent
+  },
+  {
+    path: 'detail/:airportKey',
+    component: AirportDetailComponent
   }
 ];
 
